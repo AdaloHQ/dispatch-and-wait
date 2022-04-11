ae96eae9892b49c8501109eebcd00ef7e3117333
Toby Sullivan <toby.sullivan@adalo.com>
2022-04-11 13:13:48 -0700

    Remove event filter for search

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ function find_workflow {
   while [[ true ]]
   do
     counter=$(( $counter + 1 ))
-    workflow=$(curl -s "https://api.github.com/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/runs?event=repository_dispatch" \
+    workflow=$(curl -s "https://api.github.com/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/runs" \
       -H "Accept: application/vnd.github.v3+json" \
       -H "Authorization: Bearer ${INPUT_TOKEN}" | jq '.workflow_runs[0]')
 
